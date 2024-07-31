414c14293c9d807e469bfbaa61e049a2f74ba6fc
SkSajid09 <81872093+Sajid-Kabir@users.noreply.github.com>
2024-07-31 12:24:01 +0600

    Create scriptkb1.js

---
+++ scriptkb1.js
@@ -0,0 +1,31 @@
+if() {
+    
+}else {
+    
+}const downloadBtn = document.querySelector(".download-btn");
+const fileLink = "https://drive.google.com/file/d/14cMK6089DTl1O411S7u3QjHBlTsee_cW/view?usp=drivesdk";
+
+const initTimer = () => {
+    if(downloadBtn.classList.contains("disable-timer")) {
+        return location.href = fileLink;
+    }
+    let timer = downloadBtn.dataset.timer;
+    downloadBtn.classList.add("timer");
+    downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+    const initCounter = setInterval(() => {
+        if(timer > 0) {
+            timer--;
+            return downloadBtn.innerHTML = `Your download will begin in <b>${timer}</b> seconds`;
+        }
+        clearInterval(initCounter);
+        location.href = fileLink;
+        downloadBtn.innerText = "Your file is downloading...";
+        setTimeout(() => {
+            downloadBtn.classList.replace("timer", "disable-timer");
+            downloadBtn.innerHTML = `<span class="icon material-symbols-rounded">vertical_align_bottom</span>
+                                     <span class="text">Download Again</span>`;
+        }, 3000);
+    }, 1000);
+}
+
+downloadBtn.addEventListener("click", initTimer);
